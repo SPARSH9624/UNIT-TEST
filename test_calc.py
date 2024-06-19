3fe0922fc569f71b39cfd7e888ae89e079b57877
SPARSH2433 <sparshuppal349@gmail.com>
2024-06-19 20:43:29 +0530

    Ok

--- test_calc.py
+++ test_calc.py
@@ -4,15 +4,9 @@ import calc
 class TestCalc(unittest.TestCase):
 
     def test_add(self):
-  
-        self.assertEqual(calc.add(10,5),15)
-        self.assertEqual(calc.add(-1,1),0)
-        self.assertEqual(calc.add(-1,-1),-2)
-
+      self.assertEqual(calc.add(10,5),15)
+    
     def test_subtract(self):
-
-        self.assertEqual(calc.subtract(10,5),5)
-        self.assertEqual(calc.subtract(-1,1),-2)
         self.assertEqual(calc.subtract(1,-1),2)
 
     def test_multiply(self):
@@ -23,13 +17,7 @@ class TestCalc(unittest.TestCase):
     
     def test_divide(self):
 
-        self.assertEqual(calc.divide(10,5),2)
-        self.assertEqual(calc.divide(-1,-1),1)
-        self.assertEqual(calc.divide(-1,1),-1)
-        self.assertEqual(calc.divide(5,2),2.5)
-
-        # self.assertRaises(ValueError,calc.divide,10,0) # diff syntax
-        # diff syntax for same thing as follows - 
+        self.assertEqual(calc.divide(10,5),2) 
         with self.assertRaises(ValueError):
             calc.divide(10,0)
 
